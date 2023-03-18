3e425fcc23857936550a6b93386b4b15c9d2688c
bwaklog <adityamhegde@icloud.com>
2023-03-19 03:08:33 +0530

    v 0.0.1

--- engine.py
+++ engine.py
@@ -1,7 +1,8 @@
 from operator import index
 import random as rd
+import time
 
-NUM_PARTICLES = 1
+NUM_PARTICLES = 2
 
 # define two dimentional vector
 class vector2:
@@ -10,7 +11,7 @@ class vector2:
         self.y = y
     
     def vec(self):
-        return (self.x, self.y)
+        return [self.x, self.y]
 
 # two dimentional particle
 class Particle:
@@ -21,14 +22,60 @@ class Particle:
         self.velocity = vel.vec()
         self.mass = mass
 
-particles = [[[rd.randint(0, 50), rd.randint(0, 50)], [0, 0], 1]]
+# particles = [[[rd.randint(0, 50), rd.randint(0, 50)], [0, 0], 1]]
+particles = []
 
 def PrintParticles():
-    for i in particles:
-        particle = Particle(position=i[0], velocity=i[1], mass=i[2])
+    for i in range(NUM_PARTICLES):
+        # particle = Particle(position=i[0], velocity=i[1], mass=i[2])
+        particle = particles[i]
+        print(f"Particle[{i}] {particle.position}, {particle.velocity}, {particle.mass}")
 
-        print(f"Particle[{particles.index(i)}] {particle.position}")
 
-PrintParticles()
+def InitializeParticles():
+    for i in range(NUM_PARTICLES):
+        particle = Particle(
+               position=vector2(x=rd.randint(0, 50), y=rd.randint(0, 50)).vec(),
+               velocity=vector2(x=0, y=0).vec(),
+               mass=1
+               )
+        particles.append(particle)
+
+def ComputeForce(particle):
+    force = vector2(x=0, y=particle.mass * -9.81)
+    return force.vec()
+
+def RunSimulation():
+    totalSimulationTime = 10 # simulation length is 10 seconds
+    currentTime = 0 # simulation starts at time = 0
+    dt = 1 # each step is 1 second
+
+    InitializeParticles()
+    PrintParticles()
+
+    while currentTime < totalSimulationTime:
+        time.sleep(dt)
         
+        for i in range(NUM_PARTICLES):
+            particle = particles[i]
+            force = ComputeForce(particle)
+            acceleration = vector2(
+                    x=force[0],
+                    y=force[1]/particle.mass
+                    ).vec()
+            #print(acceleration)
+            #print(f"x: {particle.position[0]}, {particle.velocity[0]}")
+            #print(f"y: {particle.position[1], particle.velocity[1]}")
+            # vel_x
+            particle.velocity[0] += acceleration[0] * dt
+            #vel_y
+            particle.velocity[1] += acceleration[1] * dt
+            # pos_x
+            particle.position[0] += particle.velocity[0] * dt
+            # pos_y
+            particle.position[1] += particle.velocity[1] * dt
 
+        PrintParticles()
+        currentTime += dt
+
+RunSimulation()
